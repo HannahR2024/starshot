f2c68a8548e8106a5f8c2735bfe12b6b89783c3c
HannahR2024 <114309364+HannahR2024@users.noreply.github.com>
2023-07-18 13:22:40 +0000

    Finished login page starting on the send page

---
+++ script.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Rabbit" && password === "123") {
+        alert("You have successfully logged in.");
+        hewy();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+function hewy(){
+  window.open("send.html", "_self");
+}
